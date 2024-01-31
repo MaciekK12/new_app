d7b7dfb98c236a769b418b0896cc9f2e005aee2b
Maciek.k12 <maciek.k1243@gmail.com>
2024-01-31 14:35:43 +0100

    Initial prisma schema and mongodb connection

--- src/app/utils/test.tsx
+++ src/app/utils/test.tsx
@@ -12,23 +12,23 @@ type UserType = {
 const MainComponent = () => {
     const [users, setUsers] = useState<UserType[]>([]);
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await fetch('/api/users');
-                if (!response.ok) {
-                    throw new Error('Błąd sieciowy lub serwera');
-                }
-                const data = await response.json();
-
-                console.log("Dane użytkowników z API:", data);
-
-                setUsers(data);
-            } catch (error) {
-                console.error("Błąd podczas pobierania danych: ", error);
+    const fetchUsers = async () => {
+        try {
+            const response = await fetch('/api/users');
+            if (!response.ok) {
+                throw new Error('Błąd sieciowy lub serwera');
             }
-        };
+            const data = await response.json();
 
+            console.log("Dane użytkowników z API:", data);
+
+            setUsers(data);
+        } catch (error) {
+            console.error("Błąd podczas pobierania danych: ", error);
+        }
+    };
+
+    useEffect(() => {
         fetchUsers();
     }, []);
 
